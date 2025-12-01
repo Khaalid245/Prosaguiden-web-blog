65cfd20368bacfd72482a9fd89182906c8949c7d
Khaalid245 <k.abdillahi@alustudent.com>
2025-12-01 20:51:18 +0530

    Setup project, add user registration, connect database

---
+++ backend/users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMIN = 'admin', 'Admin'
+        WRITER = 'writer', 'Writer'
+        READER = 'reader', 'Reader'
+
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.READER
+    )
+
+    def __str__(self):
+        return self.username
